dbd96dad5947928c4ad8d2b3902724060341ba12
fawda123 <mbafs2012@gmail.com>
2017-10-15 11:13:54 -0500

    seprate rmd files for sf and sl mods, need to figure out model sel

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,12 +1,17 @@
 library(readxl)
 library(tidyverse)
+library(lubridate)
 
 # prepping SLE data
 sl_dat <- read_excel('data/SE03_Chl_WQ.xlsx') %>% 
-  select(Date, `CHLOROPHYLL-A`, SALINITY) %>% 
+  mutate(
+    yr = year(Date)
+  ) %>% 
   rename(
     chl = `CHLOROPHYLL-A`,
     sal = SALINITY
-  )
+  ) %>% 
+  filter(yr < 2017 & yr > 1990) %>% 
+  select(Date, chl, sal)
 
 save(sl_dat, file = 'data/sl_dat.RData', compress = 'xz')
