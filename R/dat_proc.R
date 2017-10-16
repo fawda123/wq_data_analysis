9a97bd53c68e72d6486e0c6ab4f940dd09a169e7
fawda123 <mbafs2012@gmail.com>
2017-10-15 22:32:55 -0500

    added sf_fldat and sl_fldat for daily flows in sfb, sle

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -15,3 +15,17 @@ sl_dat <- read_excel('data/SE03_Chl_WQ.xlsx') %>%
   select(Date, chl, sal)
 
 save(sl_dat, file = 'data/sl_dat.RData', compress = 'xz')
+
+# prepping SLE flow data
+sl_fldat <- read_excel('data/SLE_freshwaterFlow_salinity_1989to2015.xlsx') %>% 
+  rename(
+    date = Date
+  ) %>% 
+  mutate(
+    q = `Flow (cfs)`, 
+    q = q * (0.3048^3) # cf/s to m3/s
+  ) %>% 
+  select(date, q)
+
+save(sl_fldat, file = 'data/sl_fldat.RData', compress = 'xz')
+
